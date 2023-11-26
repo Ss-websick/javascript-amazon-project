427d2dad59109159989e40b04695377830fbc642
ShivamUpadhyay2011 <helloWorldCode56@gmail.com>
2023-11-26 23:39:46 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,23 @@
 // Normalizing the data OR you can say deduplicating the data
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-}, {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+ 
+if (!cart) {
+  cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  }, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+  }];
+}
+
+// localStorage can only saves strings.
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
 // now this variable is available outside the cart.js file
-
 export function addToCart(productId) {
   let matchingItem;
 
@@ -26,6 +35,8 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+
+  saveToStorage();
  }
 
 export function removeFromCart(productId) {
@@ -38,4 +49,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
- }
+
+    saveToStorage();
+  }
