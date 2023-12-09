94485ae3dd0d9b93feb0643dcd3ae5655ca4aa61
Ss.websick <Ss.developer2011@gmail.com>
2023-12-09 20:39:38 +0530

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,18 @@
 
 // Normalizing the data OR you can say deduplicating the data
 export let cart = JSON.parse(localStorage.getItem('cart'));
+
+/* To remove from localStorage use console and type "localStorage.removeItem('cart')" */
  
 if (!cart) {
   cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
   }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 
@@ -33,7 +37,8 @@ export function addToCart(productId, selectedElementValue) {
   } else {
     cart.push({
       productId: productId,
-      quantity: selectedElementValue
+      quantity: selectedElementValue,
+      deliveryOptionId: '1'
     });
   }
 
