302c16360c88f64d547b4dbe7c62ee81cbb609b5
Ss.websick <Ss.developer2011@gmail.com>
2023-12-11 19:41:11 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,6 @@
 import { cartQuantity, renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
 cartQuantity();
 renderOrderSummary();
+renderPaymentSummary();
